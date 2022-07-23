d29b2f2a19f25282853732b1a2620a00fe39d7a9
mynk2906 <msaxena2906@gmail.com>
2022-07-23 12:11:46 +0530

    day 3 changes 23 07

---
+++ New folder/header.js
@@ -0,0 +1,12 @@
+document.querySelector('.dropdown').addEventListener('click',()=>{
+    let e1=document.querySelector('.drop-content');
+    if(e1.style.display=='block'){
+        e1.style.display='none';
+    }
+    else
+    e1.style.display='block';   
+})
+document.querySelector('#top-content').addEventListener('click',()=>{
+    let e1=document.querySelector('.drop-content');
+        e1.style.display='none';
+})
--- New folder/index.js
+++ New folder/index.js
@@ -1,4 +1,3 @@
 import {navbar} from "./components/navbar.js";
-
-
 document.getElementById("Navbar").innerHTML = navbar()
+
